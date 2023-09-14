101a3d5962c6dc4620d50de3b81e15b826c15e2c
Marcos Ricardo Rodrigues <bcc.marcos@gmail.com>
2023-09-14 18:21:09 -0400

    Update run.py
    Foi inserido comentários no código e tratativas de erros

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from app import criar_app
+from app import criar_app  # Importa a função criar_app do seu módulo app
 
-app = criar_app()
+app = criar_app()  # Cria a instância da aplicação Flask
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    try:
+        # Inicia o servidor Flask, escutando em todas as interfaces (0.0.0.0)
+        app.run(host='0.0.0.0')
+
+    except Exception as e:
+        # Trata possíveis erros e imprime uma mensagem amigável
+        print(f"Erro ao iniciar o servidor: {e}")
