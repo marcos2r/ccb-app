9e2f95d6c093dde6c49d85928fbf59e5db5073bf
Gabriel Moya <gabrielmoya123@gmail.com>
2023-11-23 19:16:10 -0400

    Altera o nome da collection

--- ccb/repositories/igreja_repository.py
+++ ccb/repositories/igreja_repository.py
@@ -7,7 +7,7 @@ from ccb.repositories.repository import Repository
 
 class IgrejaRepository(Repository):
     def __init__(self):
-        super().__init__(collection_name="igrejass", model=Type[Igreja])
+        super().__init__(collection_name="igrejas", model=Type[Igreja])
 
     def get_by_cod(self, cod: str) -> Igreja:
         result = self.collection.find_one({"cod": cod})
