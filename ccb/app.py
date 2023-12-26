0e25619403d1beed9d52f81f02a939d854648cde
Marcos Ricardo Rodrigues <bcc.marcos@gmail.com>
2023-12-26 15:45:01 -0400

    reorganizando as extensions

--- ccb/app.py
+++ ccb/app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-from dynaconf import FlaskDynaconf
+from ccb.extensions import configuration
 from ccb.extensions import database
-from ccb.blueprints import restapi
 
-
-def create_app():
-    app = Flask(__name__)
-    FlaskDynaconf(app, settings_files=["settings.toml"])
-    configuration.init_app(app)
-    database.init_app(app)
-    restapi.init_app(app)
-    return app
+app = Flask(__name__)
+configuration.init_app(app)
+database.init_app(app)
