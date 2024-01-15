c330056e19f34009320342f8d3f13f495d7cfcc1
Marcos Ricardo Rodrigues <bcc.marcos@gmail.com>
2024-01-15 13:30:08 -0400

    modificação do projeto

--- config.py
+++
@@ -1,5 +0,0 @@
-from decouple import config
-
-# Use as variáveis do seu arquivo .env para construir a mongodb_uri
-mongodb_uri = config('MONGODB_URL')
-mongodb_name = config('MONGODB_NAME')
---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from bson import json_util
+import pymongo
+import os
+
+mongodb_uri = os.environ['MONGODB_URI']
+
+client = pymongo.MongoClient(mongodb_uri)
+database = client["ccb"]
+collection = database["igrejas"]
+
+app = Flask(__name__)
+
+@app.route("/api/igrejas")
+def get_igrejas():
+    try:
+        igrejas = list(collection.find({}, {"_id": 0}))
+        return jsonify({"igrejas": json_util.dumps(igrejas)})
+    except Exception as e:
+        return jsonify({"erro": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
