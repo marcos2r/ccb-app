dc6a8cc0a778bb7387cbc889219fa2e096765f40
Marcos Ricardo Rodrigues <bcc.marcos@gmail.com>
2024-01-23 17:44:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from flask import Flask, jsonify
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pymongo import MongoClient
 import os
 
 mongodb_uri = os.getenv("MONGODB_URI")
 
-client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
+client = MongoClient(mongodb_uri)
 
 try:
     client.admin.command('ping')
 except Exception as e:
     print(f"Erro ao se conectar ao MongoDB: {e}")
 
-database = client["ccb"]
-collection = database["igrejas"]
+db = client["ccb"]
+collection = db["igrejas"]
 
 def create_app():
+    
     app = Flask(__name__)
-
-    app.config['JSON_AS_ASCII'] = False
 
     @app.route("/igrejas")
     def get_igrejas():
