8bde0a8a9d62ceceafdd8b19bf843d7c7fa040d6
Marcos Ricardo Rodrigues <bcc.marcos@gmail.com>
2024-01-26 11:32:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
